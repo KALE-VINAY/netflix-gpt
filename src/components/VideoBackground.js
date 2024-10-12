ace6e8224a0b7af3c2acfc2d8170077a6ddac369
KALE-VINAY <vinaykale321@gmail.com>
2024-10-12 22:53:47 +0530

    start time from zero

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -16,7 +16,7 @@ const VideoBackground = ({movieId}) => {
 
       <iframe 
         className='w-screen aspect-video'
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?autoplay=1&mute=1&loop=1&controls=0&color=black&modestbranding=0&playsinline=1&enablejsapi=1&start=140&rel=0&autohide=1&showinfo=0&playlist="+trailerVideo?.key }
+        src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?autoplay=1&mute=1&loop=1&controls=0&color=black&modestbranding=0&playsinline=1&enablejsapi=1&start=0&rel=0&autohide=1&showinfo=0&playlist="+trailerVideo?.key }
         title="YouTube video player" 
         
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
